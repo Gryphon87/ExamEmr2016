e10472bb811c25e2f9cb4fe1bce8c6f225939c55
gryphon87 <Filippo.Bozzola@gmail.com>
2016-07-12 15:44:43 +0200

    bug fix

--- reducer.py
+++ reducer.py
@@ -4,14 +4,16 @@ from collections import defaultdict
 
 
 def reduce():
-	my_dict = defaultdict(set)	
+	results = defaultdict(set)	
 	for pipe in sys.stdin:
 		line = pipe.strip('\n').split('\t')
 		machine = line[0]
 		time = int(line[1])
 		#add the key to the dictionary, adding or incrementing the time value
-		my_dict[machine].add(time + int(my_dict.get(machine))
-	print my_dict
+		results[machine].add(time + int(results.get(machine)))
+
+	for key in results:
+		print('Macchina ' + key + ': ' + str(results[key]))
 
 if __name__ == '__main__':
 	reduce()
