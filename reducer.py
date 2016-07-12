3e0d5464f4866de9e69bc33bc31a3b393c085c29
gryphon87 <Filippo.Bozzola@gmail.com>
2016-07-12 15:23:26 +0200

    Fixed problem with string concatenate, casted to int the int value of time

--- reducer.py
+++ reducer.py
@@ -9,10 +9,10 @@ def reduce():
 	for pipe in sys.stdin:
 		line = pipe.strip()
 		machine = line.split('\t')[0]
-		time = line.split('\t')[1]
+		time = int(line.split('\t')[1])
 		
 		#add the key to the dictionary, adding or incrementing the time value
-		my_dict[machine].add(time + my_dict[machine])
+		my_dict[machine].add(time + int(my_dict[machine]))
 
 	for key in my_dict:
 		print key + '\t' + s
